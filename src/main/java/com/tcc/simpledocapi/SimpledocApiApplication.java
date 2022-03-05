60b39f87485e75a401582a9e6ac2af28c1d7e2c4
Dumilde Matos <dumildematos@Dumildes-MacBook-Pro.local>
2022-03-05 20:46:02 +0100

    first commit

---
+++ src/main/java/com/tcc/simpledocapi/SimpledocApiApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.simpledocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpledocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpledocApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/simpledocapi/SimpledocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.simpledocapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpledocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
