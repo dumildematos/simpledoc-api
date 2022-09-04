e554615541c755b8013fa4d50ec63461f0907986
Dumilde Matos <dumildejosematos@gmail.com>
2022-09-04 19:49:09 +0100

    build locally

--- src/main/java/com/tcc/simpledocapi/config/security/WebSecurityConfig.java
+++ src/main/java/com/tcc/simpledocapi/config/security/WebSecurityConfig.java
@@ -14,12 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.ServletException;
 import java.util.Arrays;
 
 import static org.springframework.http.HttpMethod.GET;
