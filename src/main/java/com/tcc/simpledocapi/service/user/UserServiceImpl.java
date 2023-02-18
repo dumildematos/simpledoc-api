736659279f6608c74938628438fb86b3ec86b5db
dumilde_jose_matos <dmatos@cpiretail.pt>
2023-02-18 10:03:36 +0000

    encode password

--- src/main/java/com/tcc/simpledocapi/service/user/UserServiceImpl.java
+++ src/main/java/com/tcc/simpledocapi/service/user/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,12 +29,13 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public User saveUser(User user, String roleName) {
 
 
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
         Role role = roleRepository.findByName(roleName);
         user.getRoles().clear();
