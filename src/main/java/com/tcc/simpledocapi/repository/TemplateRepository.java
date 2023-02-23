3258338d55dfa3bde141af6a0746e78a5717734a
Dumilde Matos <dmatos@cpiretail.pt>
2023-02-23 23:52:22 +0100

    update

--- src/main/java/com/tcc/simpledocapi/repository/TemplateRepository.java
+++ src/main/java/com/tcc/simpledocapi/repository/TemplateRepository.java
@@ -16,10 +16,10 @@ public interface TemplateRepository extends JpaRepository<Template, Long> {
     @Query(value = "select t.id, t.content, t.created_at,t.name, t.description, t.price, t.cover from template t left join user_templates ut on t.id = ut.templates_id inner join user u on ut.user_id = u.id and u.id = ?1 and t.name like concat('%', ?2 ,'%')", nativeQuery = true)
     Page<Template> findUserTemplate(@Param("userId") Long userId, @Param("tName") String name, Pageable pageable);
 
-        @Query(value = "select t.id, t.content, t.created_at,t.name, t.price, t.cover, t.description from template t  join template_category tc on tc.template_id = t.id and tc.category_id = ?1  join user_templates ut on t.id = ut.templates_id  join user u on u.id = ut.user_id  join user_roles ur on u.id = ur.user_id and ur.roles_id = 1  and t.name like concat('%', ?2 ,'%') group by t.id", nativeQuery = true)
+        @Query(value = "select t.id, t.content, t.created_at,t.name, t.price, t.cover, t.description from template t  join template_category tc on tc.template_id = t.id and tc.category_id = ?1  join user_templates ut on t.id = ut.templates_id  join user u on u.id = ut.user_id  join user_roles ur on u.id = ur.user_id join role r on r.id = ur.roles_id and r.name = 'ROLE_ADMIN'  and t.name like concat('%', ?2 ,'%') group by t.id", nativeQuery = true)
     Page<Template> findAdminTemplate(@Param("categoryId") Long categoryId, @Param("tName") String tName, Pageable pageable);
 
-    @Query(value = "select t.id, t.content, t.description, t.created_at,t.name, t.price, t.cover from template t join user_templates ut on t.id = ut.templates_id  join user u on u.id = ut.user_id  join user_roles ur on u.id = ur.user_id and ur.roles_id = 1 and t.name like concat('%', ?1 ,'%') group by t.id", nativeQuery = true)
+    @Query(value = "select t.id, t.content, t.description, t.created_at,t.name, t.price, t.cover from template t join user_templates ut on t.id = ut.templates_id  join user u on u.id = ut.user_id  join user_roles ur on u.id = ur.user_id join role r on r.id = ur.roles_id and r.name = 'ROLE_ADMIN' and t.name like concat('%', ?1 ,'%') group by t.id", nativeQuery = true)
     Page<Template> findAllAdminTemplate(@Param("tName") String tName, Pageable pageable);
 
     @Modifying
